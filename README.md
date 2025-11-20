aa3058d2faf9eda1e3289083d21ecb154625e5ea
Nick Maclean <nick@imaclean.me>
2025-11-20 17:23:39 -0600

    transitioned from 2nd to 3rd rendering!

--- README.md
+++ README.md
@@ -12,10 +12,24 @@ Dependencies
 1. cd into a project directory (ppm-generator, tracer, ...)
 2. `cmake -B build`
 3. `ninja -C build`
-4. `"build/{PROJECT}.exe"
+4. `"build/{PROJECT}.exe"`
 5. That's it!
+
+---
 
 ## Progress Notes
 
-11/19 9am:    finished section 1, 2 (environment setup and ppm-generator/main.cpp)
-11/20 9:46am: finished section 3    (tracer/vec3.h and color.h)
+11/20 5:20pm
+
+- finished section 4
+- setup camera for main loop (renders a skybox) and tracer/ray.h
+
+11/20 9:46am
+
+- finished section 3
+- render is the same but tracer/vec3.h and color.h
+
+11/19 9am
+
+- sections 1, 2
+- environment setup and basic image renderer in ppm-generator/main.cpp
--- tracer/main.cpp
+++ tracer/main.cpp
@@ -2,22 +2,51 @@
 
 #include "vec3.h"
 #include "color.h"
+#include "ray.h"
 
+
+color ray_color(const ray& r)
+{
+    vec3 dir = unit_vector(r.direction());
+    double a = 0.5*(dir.y()+1.0);
+    return (1-a)*color(1, 1, 1) + a*color(0.5, 0.7, 1);
+}
 
 int main()
 {
-    int width = 256;
-    int height = 256;
+    // calculate screen size values
+    double aspect_ratio = 16.0 / 9.0;
+    int width  = 256;
+    int height = int(width / aspect_ratio);
+    height = (height < 1) ? 1 : height;
+    double vheight = 2;
+    double vwidth  = vheight * (double(width) / height);
 
+    // camera settings
+    double focal_length = 1.0;
+    vec3 camera_position = point3(0, 0, 0);
+
+    // setup maths for translating screen to viewport position
+    vec3 viewport_u = vec3(vwidth, 0, 0);
+    vec3 viewport_v = vec3(0, -vheight, 0);
+    vec3 viewport_du = viewport_u / width;
+    vec3 viewport_dv = viewport_v / height;
+
+    // calculate top-left position in-world
+    vec3 viewport_topleft = camera_position - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+    vec3 viewport_topleft_px = viewport_topleft + 0.5*(viewport_du + viewport_dv);
+
+    // render!
     std::cout << "P3\n" << width << " " << height << "\n255\n";
-
-    for (int y = 0; y < height; y++)
+    for (int v = 0; v < height; v++)
     {
-	std::clog << "\rScanlines remaining: " << (height - y) << std::flush;
-	for (int x = 0; x < width; x++)
+	std::clog << "\rScanlines remaining: " << (height - v) << "   " << std::flush;
+	for (int u = 0; u < width; u++)
 	{
-	    auto pixel = color(double(x) / (width = 1), double(y) / (height - 1), 0);
-	    write_color(std::cout, pixel);
+	    vec3 pixel_center = viewport_topleft_px + u*viewport_du + v*viewport_dv;
+	    ray cam_to_px = ray(camera_position, pixel_center - camera_position);
+	    color pixel_color = ray_color(cam_to_px);
+	    write_color(std::cout, pixel_color);
 	}
     }
 

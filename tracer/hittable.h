1aa4c95927dec7add4f450e60771e67d029f799b
Nick Maclean <nick@imaclean.me>
2025-11-25 08:58:32 -0600

    abstracted geometry and collision for sphere
    extended geometry abstraction to contain a scene
    
    update readme

---
+++ tracer/hittable.h
@@ -0,0 +1,28 @@
+#ifndef HIITABLE_H
+#define HIITABLE_H
+
+#include "tracer.h"
+
+class hit_info {
+public:
+  point3 p;
+  vec3 normal;
+  double t;
+  bool front_face;
+
+  void set_face_normal(const ray &r, const vec3 &outward_normal) {
+    // outward should be normallzed
+    front_face = dot(r.direction(), outward_normal) < 0;
+    normal = front_face ? outward_normal : -outward_normal;
+  }
+};
+
+class hittable {
+public:
+  virtual ~hittable() = default;
+
+  virtual bool hit(const ray &r, interval clipping_range,
+                   hit_info &info) const = 0;
+};
+
+#endif
